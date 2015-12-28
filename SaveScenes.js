7b393bad4058ffadd5be74ba91e1a82b5d79cf90
OriginalSin <saleks@scanex.ru>
2015-12-28 19:46:40 +0300

    update

--- SaveScenes.js
+++ SaveScenes.js
@@ -5,113 +5,117 @@
 
     _translationsHash.addtext('rus', {
         'SaveScenes.iconTitle'  : 'Поиск снимков по экрану',
-        'SaveScenes.setTop'     : 'Показать/Скрыть снимок'
+        'SaveScenes.setTop'     : 'Снимков:',
+        'SaveScenes.visible'    : 'видимых:'
     });
     _translationsHash.addtext('eng', {
         'SaveScenes.iconTitle'  : 'Find scene by screen',
-        'SaveScenes.setTop'     : 'Show/Hide quicklook'
+        'SaveScenes.setTop'     : 'Quicklook:',
+        'SaveScenes.visible'    : 'on:'
     });
 
-var setLayers = function(map) {
-// В маплете карты
-    var setHiddenLayer = function(layer) {
+    var setHiddenLayer = function(layer, map) {
         var ids = {};
         if (L.Mixin.ContextMenu) {
+console.log('ddddddddddd', layer._gmx.layerID);
             L.setOptions(layer, {
                 contextmenu: false,
-                contextmenuItems: [{
-                    text: 'Center map here',
-                    callback: function () {console.log(arguments);}
-                }],
+                contextmenuItems: [
+                    { separator: true },
+                    { text: '' },
+                    { text: '' }
+                ],
                 contextmenuInheritItems: true,
             });
             L.extend(layer, L.Mixin.ContextMenu);
             layer.bindContextMenu();
-        }
-        layer
-            .on('mousedown', function(ev) {
-                var out = {};
-                var res = ev.gmx.targets.map(function(it) {
-                    var propsArr = it.properties,
+
+            var select = L.DomUtil.create('select', 'select-items');
+            L.DomEvent
+                .on(select, 'mousewheel', L.DomEvent.stopPropagation)
+                .disableScrollPropagation(select);
+
+            select.multiple = true;
+            select.size = 6;
+
+            var setCount = function(all, visible) {
+                var contextmenu = map.contextmenu,
+                    arr = contextmenu._items;
+
+                arr[arr.length - 2].el.innerHTML = _gtxt(pluginName + '.setTop') +
+                    ' <b>' + all + '</b> ' +
+                    _gtxt(pluginName + '.visible') +
+                    ' <b>' + visible + '</b>';
+            };
+
+            var selectItem = L.bind(function (ev) {
+                var count = 0,
+                    target = ev.target,
+                    len = target.options.length;
+                for (var i = 0; i < len; i++) {
+                    var option = target.options[i];
+                    if (option) {
+                        var id = option._id;
+                        if (option.selected) {
+                            if (!ids[id]) {
+                                ids[id] = true;
+                                layer.bringToTopItem(id);
+                                layer.redrawItem(id);
+                            }
+                            count++;
+                        } else {
+                            if (ids[id]) {
+                                ids[id] = false;
+                                layer.redrawItem(id);
+                            }
+                        }
+                    }
+                }
+                setCount(len, count);
+// console.log('change', option, select.selectedIndex, ids);
+            }, this);
+            L.DomEvent.on(select, 'change', selectItem);
+
+            var addOptions = function(arr) {
+                var count = 0, i,
+                    len = arr.length;
+                for (i = select.options.length - 1; i >= 0; i--) {
+                    select.remove(i);
+                }
+                for (i = 0; i < len; i++) {
+                    var it = arr[i],
+                        propsArr = it.properties,
                         properties = layer.getItemProperties(propsArr),
                         sceneid = properties.sceneid,
-                        geometry = [propsArr[propsArr.length - 1]];
+                        option = document.createElement('option');
 
-                    var mItem = {
-                        //text: '<b>' + sceneid + '</b>',
-                        text: '<option>' + sceneid + '</option>',
-                        //callback: function () { selectItem(sceneid); }
-                    };
-                    if (out[sceneid]) {
-                        mItem.icon = 'images/error.png';
-                        // console.log('Error: ', sceneid);
-                    } else {
-                        out[sceneid] = {
-                            properties: properties,
-                            sceneid: sceneid,
-                            geometry: geometry
-                        };
-                    }
-                    return '<option>' + sceneid + '</option>';
-                });
-                layer.options.contextmenuItems = [
-                    { separator: true },
-                    {
-                        text: '<select class="select-item"><option>' + _gtxt(pluginName + '.setTop') + '</option>' + res + '</select>'
-                        // ,
-                        // context: this
-                        // ,
-                        // callback: function () {
-                    // console.log('callback', arguments);
-                        // }
-                    }
-                ];
-                var select = null;
-                var selectItem = function (ev) {
-                    var sel = ev.target;
-                    if (sel.selectedIndex) {
-                        var current = sel.options[sel.selectedIndex];
-                        var sceneid = current.value;
-console.log('change', sceneid, current.selected);
-                    }
-                };
-                
-                map.on('contextmenu.hide', function (ev) {
-                    if (select) { L.DomEvent.off(select, 'change', selectItem); }
-                });
-                map.on('contextmenu.show', function (ev) {
+                    option.text = sceneid;
+                    option._id = it.id;
+                    option.selected = ids[it.id];
+                    if (option.selected) { count++; }
+                    select.add(option);
+                }
+                setCount(len, count);
+var tt = 1;
+            };
+            layer
+                .setStyleHook(function(it) {
+                    return ids[it.id] ? {} : {skipRasters: true};
+                })
+                .on('contextmenu', function (ev) {
                     var contextmenu = map.contextmenu,
-                        items = contextmenu._items,
-                        el = items[items.length - 1].el;
-                    // contextmenu.setDisabled(items[items.length - 1], true);
-                    L.DomUtil.addClass(el, 'leaflet-contextmenu-item-disabled');
-                    select = el.childNodes[0]
-                    L.DomEvent.on(select, 'change', selectItem);
-                    // console.log('contextmenu.show', arguments);
-                });
-                        
-                // console.log('mousedown', res, ev.gmx.targets);
-            })
-            .on('click', function(ev) {
-                var id = ev.gmx.id;
-                ids[id] = !ev.originalEvent.altKey;
-                layer.redrawItem(id);
-            })
-            .setStyleHook(function(it) {
-                return ids[it.id] ? {} : {skipRasters: true};
-            });
-    };
+                        arr = contextmenu._items,
+                        elSum = arr[arr.length - 2].el,
+                        el = arr[arr.length - 1].el;
 
- console.log('ddddddd', nsGmx.gmxMap, map);
-    for (var layerID in nsGmx.gmxMap.layersByID) {
-        var layer = nsGmx.gmxMap.layersByID[layerID],
-            rawProps = layer.getGmxProperties(),
-            viewType = rawProps.MetaProperties.viewType;
-        if (viewType && viewType.Value === 'hidden') {
-            setHiddenLayer(layer);
+                    L.DomUtil.addClass(elSum, 'leaflet-contextmenu-item-disabled');
+                    L.DomUtil.addClass(el, 'leaflet-contextmenu-item-disabled');
+                    el.appendChild(select);
+                    addOptions(ev.gmx.targets);
+    // console.log('__layer_____', layer.options.contextmenuItems, arr);
+                });
         }
-    }
-};
+    };
 
     var publicInterface = {
         pluginName: pluginName,
@@ -121,8 +125,15 @@ console.log('change', sceneid, current.selected);
                 regularImage: 'satellite.png',
                 layerName: null
             }, params);
-            
-            setLayers(nsGmx.leafletMap);
+
+            for (var layerID in nsGmx.gmxMap.layersByID) {
+                var layer = nsGmx.gmxMap.layersByID[layerID],
+                    rawProps = layer.getGmxProperties(),
+                    viewType = rawProps.MetaProperties.viewType;
+                if (viewType && viewType.Value === 'hidden') {
+                    setHiddenLayer(layer, nsGmx.leafletMap);
+                }
+            }
             
             var icon = new L.Control.gmxIcon({
                 id: pluginName, 
@@ -215,11 +226,10 @@ if (out.length) {
         }
     });
 }
-
 */
+
             });
             nsGmx.leafletMap.addControl(icon);
-
         }
     };
     gmxCore.addModule(pluginName, publicInterface, {
